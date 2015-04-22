26e1f8e6fad00877f67b22846ccf7214534906e4
Sara OC <saramoo@hotmail.com>
2015-04-22 14:12:59 +0100

    board fills with cells appropriate to size

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,11 @@
 class Board
 
-  attr_reader :size
+  attr_reader :size, :cells
 
   def initialize size
     @size = size
+    @cells = []
+    @size.times { @cells << Cell.new }
   end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,9 +2,15 @@ require 'board'
 
 describe Board do
   let(:board) { Board.new(1) }
+  # let(:cell) { double :cell, holds: 'water', is_hit: false }
 
   it 'has a size' do
     expect(board.size).to eq(1)
+  end
+
+  it 'is filled with cells' do
+    p board.cells
+    expect(board.cells.count).to eq(1)
   end
 
 end
